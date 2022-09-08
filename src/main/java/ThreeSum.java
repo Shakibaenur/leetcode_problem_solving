5f278fe2e4b51552353da288fb03d7ff9c30a89d
shakibaenur <shakibaenur2612@gmail.com>
2022-09-08 10:05:58 +0600

    LeetCode Problem Solving Started.... 1. Roman to Integer Solved. 2. Longest Common Prefix completed

---
+++ src/main/java/ThreeSum.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] array={-1,0,1,2,-1,-4};
+        System.out.println(threeSum(array));
+    }
+
+    private static List<List<Integer>> threeSum(int[] array) {
+        Arrays.sort(array);
+
+    }
+}
